3dd627cde04dc5b1158229b0acbebaac51e037a7
i520java_jinge <i520java@126.com>
2019-09-20 12:25:04 +0800

    dock构建完成

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+export PORT=${PORT:-5601}
+export ES_URL=${ES_URL:-"http://localhost:9200"}
+ 
+echo "server.host: \"0\""
+echo "server.host: \"0\"" >> ${KIBANA_HOME}/config/kibana.yml
+echo "server.name: kibana-logging"
+echo "server.name: kibana-logging" >> ${KIBANA_HOME}/config/kibana.yml
+echo "server.port: ${PORT}"
+echo "server.port: ${PORT}" >> ${KIBANA_HOME}/config/kibana.yml
+echo "elasticsearch.url: ${ES_URL}"
+echo "elasticsearch.url: ${ES_URL}" >> ${KIBANA_HOME}/config/kibana.yml
+ 
+sh ${KIBANA_HOME}/bin/kibana
